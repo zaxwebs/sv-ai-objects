3a453555446d4ad6439b6bab03565882e96e4b09
zaxwebs <zifidigital@gmail.com>
2025-03-28 05:57:11 +0530

    ui updates

--- src/routes/movie-recommendation/+page.svelte
+++ src/routes/movie-recommendation/+page.svelte
@@ -9,12 +9,13 @@
 		api: '/api/movie-recommendation',
 		schema: movieSchema,
 	})
+
 	let input = $state('')
 	let userMessage = $state('')
 
-	function handleSubmit(e: Event) {
+	function handleSubmit(event: Event) {
+		event.preventDefault()
 		userMessage = input
-		e.preventDefault()
 		structuredObject.submit(input)
 		input = ''
 	}
@@ -24,38 +25,42 @@
 	<title>Flix AI</title>
 </svelte:head>
 
-<main class="max-w-[1400px] mx-auto">
-	<section class="p-4 py-8 flex flex-col items-center max-w-xl mx-auto text-center">
-		<div class="flex items-center gap-2 mb-6">
-			<img class="w-14" src="/images/popcorn_color.svg" alt="popcorn" />
-			<h1 class="text-4xl font-bold font-[boldonse] text-pink-600">Flix AI</h1>
-		</div>
+<header class="bg-white">
+	<div class="max-w-[1400px] mx-auto p-3 flex justify-between items-center">
+		<img class="w-8" src="/images/popcorn_color.svg" alt="Popcorn logo" />
+		<a class="text-slate-600 hover:underline" href="https://www.instagram.com/zaxwebs"
+			>@zaxwebs</a
+		>
+	</div>
+</header>
+
+<main class="max-w-[1400px] mx-auto py-16">
+	<section class="p-4 flex flex-col items-center max-w-xl mx-auto text-center">
+		<h1 class="text-3xl font-[boldonse] text-pink-600 mb-6 leading-[1.4]">
+			AI Movie Recommendations
+		</h1>
 		<p class="text-slate-700">
-			Need movie recommendations? Simply tell us what you're in the mood for and we'll take
-			care of the rest. You can ask for recommendations based on genres, actors, or even
-			specific movies — just like asking a friend.
+			Need movie recommendations? Just tell us what you're in the mood for, and we'll take
+			care of the rest. Get suggestions based on genres, actors, or even specific movies—just
+			like asking a friend!
 		</p>
 	</section>
+
 	<section class="p-4 mb-6">
-		<form
-			class="flex gap-4 max-w-3xl mx-auto"
-			onsubmit={(e) => {
-				e.preventDefault()
-				handleSubmit(e)
-			}}
-		>
+		<form class="flex gap-4 max-w-3xl mx-auto" on:submit={handleSubmit}>
 			<Input
 				type="text"
-				placeholder="What kinda movie would you like to watch?"
+				placeholder="What kind of movie would you like to watch?"
 				bind:value={input}
 			/>
 			<Button type="submit" disabled={structuredObject.loading}>Get Recommendations</Button>
 		</form>
 	</section>
+
 	{#if structuredObject.object?.movies}
 		<section class="p-4">
 			<div class="grid grid-cols-2 gap-4">
-				{#each structuredObject.object?.movies ?? [] as movie, i (i)}
+				{#each structuredObject.object.movies as movie, i (i)}
 					<RecommendationCard {movie} />
 				{/each}
 			</div>
